a91cfcbded4b18e89e9c95fc3e5b0ed8f430a0d1
Vivi <vivi@privacyrequired.com>
2024-11-08 16:44:52 -0500

    Update Astro configuration for Vercel deployment and add Vercel analytics package

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,14 @@ import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compress from "@playform/compress";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://landingpad.cosmicthemes.com/",
-  output: "hybrid",
-  adapter: netlify({
-    imageCDN: false,
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
   }),
   redirects: {
     "/admin": "/keystatic",
